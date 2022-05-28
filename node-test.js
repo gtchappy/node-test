563361f8b7517b5f59605661469e3c14f62ba698
gtchappy <2425172709@qq.com>
2022-05-28 10:33:56 +0800

    添加了爱心

--- node-test.js
+++ node-test.js
@@ -3,16 +3,16 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
   console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
   process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
   var parsedUrl = url.parse(request.url, true)
-  var pathWithQuery = request.url 
+  var pathWithQuery = request.url
   var queryString = ''
-  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  if (pathWithQuery.indexOf('?') >= 0) { queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
   var path = parsedUrl.pathname
   var query = parsedUrl.query
   var method = request.method
@@ -21,15 +21,113 @@ var server = http.createServer(function(request, response){
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
 
-  if(path === '/'){
+  if (path === '/') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/html;charset=utf-8')
-    response.write(`金女士我喜欢你(●'◡'●)`)
+    response.write(`
+
+    <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/x" 
+</head>
+    
+    <body>
+    <div><p>金女士我喜欢你(●'◡'●)</p></div>
+    <div id="heart">
+   
+
+        <div class="left"></div>
+        <div class="right"></div>
+        <div class="bottom"></div>
+    </div>
+</body>
+
+</html>
+    
+    
+    `)
     response.end()
-  } else if(path === '/x'){
+  } else if (path === '/x') {
     response.statusCode = 200
     response.setHeader('Content-Type', 'text/css;charset=utf-8')
-    response.write(`body{color: red;}`)
+    response.write(`
+    * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+}
+
+#heart {
+    position: relative;
+    margin: 100px;
+    /* border: 1px solid red; */
+    display: inline-block;
+    /* left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);  */
+    /* transition: all .5s; */
+    animation: xxx 0.7s 1s alternate infinite;
+    
+}
+
+@keyframes xxx {
+    0% {
+        transform: scaleX(1);
+    }
+
+    50% {
+        transform: scale(1.5);
+    }
+
+    100% {
+        transform: scale(1) scale(1.5);
+    }
+
+}
+
+#heart>.bottom {
+    width: 50px;
+    height: 50px;
+    /* border: 1px solid rgb(18, 85, 228); */
+    background-color: red;
+    transform: rotate(45deg);
+    /* position: absolute; */
+}
+
+
+
+#heart > .left {
+    width: 50px;
+    height: 50px;
+    /* border: 1px solid rgb(21, 192, 222); */
+    background-color: red;
+    /* border-radius: 50%; */
+    position: absolute;
+    bottom: 100%;
+    left: 100%;
+    transform: rotate(45deg) translateY(41px);
+    border-radius: 50% 50% 0 0;
+}
+
+#heart>.right {
+    width: 50px;
+    height: 50px;
+    /* border: 1px solid rgb(215, 16, 16); */
+    background-color: red;
+    /* border-radius: 50%; */
+    position: absolute;
+    bottom: 100%;
+    right: 100%;
+    transform: rotate(45deg) translateX(41px);
+    border-radius: 50% 0 0 50%;
+}
+    
+    `)
     response.end()
   } else {
     response.statusCode = 404
